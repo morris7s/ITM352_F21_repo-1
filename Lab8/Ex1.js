0421f1ece210a1ea61852269208b2a83b35325f2
alyssia-chen <84742988+alyssia-chen@users.noreply.github.com>
2021-10-01 21:53:18 -1000

    Lab8

---
+++ Lab8/Ex1.js
@@ -0,0 +1,26 @@
+require("./products_data.js");
+
+var num_products = 5;
+
+var count = 0;
+while (count < num_products) {
+    count++;
+    if (count > num_products/4 && count <= (num_products/4) * 3) {
+        console.log(`${count}. ${eval('name' + count) + " is sold out!"}`);
+        continue;
+    }
+    console.log(`${count}. ${eval('name' + count)}`);
+}
+console.log("That's all we have!");
+
+
+// var num_products = 5;
+
+for (let count = 1; count < (num_products + 1); count++) {
+    if (count > num_products / 4 && count <= (num_products / 4) * 3) {
+        console.log(`${count}. ${eval('name' + count) + " is sold out!"}`);
+        continue;
+    }
+    console.log(`${count}. ${eval('name' + count)}`);
+}
+console.log("That's all we have!");
