e40b1263fd39878ea1c163caf6b11d6eea61aab7
alyssia-chen <84742988+alyssia-chen@users.noreply.github.com>
2021-11-05 18:53:30 -1000

    Lab13

---
+++ Lab13/info_server_EC.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var app = express();
+var myParser = require("body-parser");
+
+var products = require('./product_data.json');
+products.forEach( (prod,i) => {prod.total_sold = 0});
+products.forEach( (prod,i) => {prod.errors = ""});
+
+
+app.get("/product_data.js", function (request, response, next) {
+    response.type('.js');
+    var products_str = `var products = ${JSON.stringify(products)};`;
+    response.send(products_str);
+});
+
+
+function isNonNegativeInteger(inputString, returnErrors = false) {
+    // Validate than an input value is a non-negative integer
+    // inputString is the input stringl returnErrors indicates how the function returns: true means return 
+    // the array and false means return a boolean
+    errors = []; // assume no errors at first
+    if (Number(inputString) != inputString) {
+        errors.push("Not a number!"); // Check if string is a number value
+    } else {
+        if (inputString < 0) {
+            errors.push("Negative value!"); // Check if it is non-negative
+        }
+        if (parseInt(inputString) != inputString) {
+            errors.push("Not an integer!"); // Check that it is an integer
+        }
+    }
+    return returnErrors ? errors : (errors.length == 0)
+}
+
+app.all('*', function (request, response, next) { // For all request types (GET and POST), respond to any path that matches this first parameter (* means everything) by executing this arrow function
+    console.log(request.method + ' to path ' + request.path);
+    next();
+    console.log("1");
+});
+
+// Rule to handle process_form request from order_page.html
+app.use(myParser.urlencoded({ extended: true }));
+
+app.post("/process_form", function (request, response) {
+    receipt = '';
+    let qtys = request.body[`quantity_textbox`];
+    for (i in qtys) {
+        q = qtys[i];
+        let brand = products[i]['brand'];
+        let brand_price = products[i]['price'];
+        if (isNonNegativeInteger(q)) {
+            products[i]['total_sold'] += Number(q);
+            receipt += `<h3>Thank you for purchasing: ${q} ${brand}. Your total is \$${q * brand_price}!</h3>`; // render template string
+            products["errors"] = "";
+        } else {
+            receipt += `<h3><font color="red">${q} is not a valid quantity for ${brand}!</font></h3>`;
+            products["errors"] = `${q} is an invalid quantity!`;
+        }
+    }
+    response.send(receipt);
+    response.end();
+
+});
+
+app.get('/test', function (request, response, next) { // For all request types (GET and POST), respond to any path that matches this first parameter (* means everything) by executing this arrow function
+    response.send("Got a GET request to path: test");
+    console.log("2");
+});
+
+app.use(express.static('./public'));
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here, when you're listening, exectue the arrow function
+
+// next says execute the next path that matches the request; lets you move to the next rule
---
+++ Lab13/info_server_Ex2a.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.all('*', function (request, response, next) { // For all request types (GET and POST), respond to any path that matches this first parameter (* means everything) by executing this arrow function
+    console.log(request.method + ' to path ' + request.path);
+    next();
+    console.log("1");
+});
+
+app.get('/test', function (request, response, next) { // For all request types (GET and POST), respond to any path that matches this first parameter (* means everything) by executing this arrow function
+    response.send("Got a GET request to path: test");
+    console.log("2");
+});
+
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here, when you're listening, exectue the arrow function
+
+// next says execute the next path that matches the request; lets you move to the next rule
